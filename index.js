df111884a3bd81ee18bbbe1a212db4f174a6cad9
Atit Kharel <atit286@gmail.com>
2022-08-06 19:19:57 +0545

    initial commit

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('Hello World!!');
+});
+
+app.get('/api/courses', (req, res) => {
+    res.send([1, 2, 3]);
+});
+
+// PORT
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+    console.log(`Listening on port ${port}`);
+});
