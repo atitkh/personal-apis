cfbe794b9b0692eb310d0665c90c7b5955044227
Atit Kharel <atit286@gmail.com>
2022-08-08 00:10:56 +0545

    added individual endpoints

--- routes/nft/nftsearch.js
+++ routes/nft/nftsearch.js
@@ -7,13 +7,14 @@ router.get('/', (req, res) => {
     res.send('Welcome to NFT Search API');
 });
 
+// all nft listings
 router.get('/all', verify, async (req, res) => {
     assets = [];
     if (req.query.address) {
         try {
             address = req.query.address;
 
-            //polygon chain code
+            //polygon chain 
             let url = `https://api.nftport.xyz/v0/accounts/${address}?chain=polygon`;
             let options = {
                 method: 'GET',
@@ -23,7 +24,7 @@ router.get('/all', verify, async (req, res) => {
                 }
             };
 
-            //ethereum chain code
+            //ethereum chain 
             let url2 = `https://api.nftport.xyz/v0/accounts/${address}?chain=ethereum`;
             let options2 = {
                 method: 'GET',
@@ -52,6 +53,90 @@ router.get('/all', verify, async (req, res) => {
 
             for (let j = 0; j < data_ethereum.length; j++) {
                 let nft = data_ethereum[j];
+                if (nft.file_url) {
+                    nft.file_url = ipfsChecker(nft.file_url);
+                }
+                assets.push(nft);
+            }
+
+            total_nfts = assets.length;
+            assets.push({ "total_nfts" : total_nfts });
+
+            res.send(assets);
+        } catch (error) {
+            res.send(error);
+        }
+
+    } else {
+        res.status(400).send('Address is required');
+    }
+});
+
+// polygon chain only
+router.get('/polygon', verify, async (req, res) => {
+    assets = [];
+    if (req.query.address) {
+        try {
+            address = req.query.address;
+
+            //polygon chain 
+            let url = `https://api.nftport.xyz/v0/accounts/${address}?chain=polygon`;
+            let options = {
+                method: 'GET',
+                headers: {
+                    'Content-Type': 'application/json',
+                    Authorization: `${process.env.NFTPORTAL_KEY}`
+                }
+            };
+
+            let response_polygon = await fetch(url, options);
+            let nfts_polygon = await response_polygon.json();
+            let data_polygon = nfts_polygon.nfts;
+
+            for (let i = 0; i < data_polygon.length; i++) {
+                let nft = data_polygon[i];
+                if (nft.file_url) {
+                    nft.file_url = ipfsChecker(nft.file_url);
+                }
+                assets.push(nft);
+            }
+
+            total_nfts = assets.length;
+            assets.push({ "total_nfts" : total_nfts });
+
+            res.send(assets);
+        } catch (error) {
+            res.send(error);
+        }
+
+    } else {
+        res.status(400).send('Address is required');
+    }
+});
+
+//etherum chain only
+router.get('/ethereum', verify, async (req, res) => {
+    assets = [];
+    if (req.query.address) {
+        try {
+            address = req.query.address;
+
+            //ethereum chain 
+            let url = `https://api.nftport.xyz/v0/accounts/${address}?chain=ethereum`;
+            let options = {
+                method: 'GET',
+                headers: {
+                    'Content-Type': 'application/json',
+                    Authorization: `${process.env.NFTPORTAL_KEY}`
+                }
+            };
+
+            let response_ethereum = await fetch(url, options);
+            let nfts_ethereum = await response_ethereum.json();
+            let data_ethereum = nfts_ethereum.nfts;
+
+            for (let i = 0; i < data_ethereum.length; i++) {
+                let nft = data_ethereum[i];
                 if (nft.file_url) {
                     nft.file_url = ipfsChecker(nft.file_url);
                 }
