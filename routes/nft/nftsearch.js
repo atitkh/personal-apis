a9b6b77031756be12df38afee1e69052f5262506
Atit Kharel <atit286@gmail.com>
2022-08-07 22:41:33 +0545

    changed fetch version

--- routes/nft/nftsearch.js
+++ routes/nft/nftsearch.js
@@ -12,7 +12,23 @@ router.get('/all', verify, (req, res) => {
     {'ethereum':''}];
     if(req.query.address) {
         address = req.query.address;
-        console.log(address);
+        let url = `https://api.nftport.xyz/v0/accounts/${address}`;
+        let options = {
+            method: 'GET',
+            qs: {chain: 'polygon'},
+            headers: {
+              'Content-Type': 'application/json',
+              Authorization: `${process.env.NFTPORTAL_KEY}`
+            }
+          };
+
+        fetch(url, options)
+        .then(res => res.json())
+        .then(json => {
+            assets.polygon = json.nfts;
+            console.log(assets.polygon);
+        })
+        .catch(err => console.error('error:' + err));
         
         //make api call to nftport.xyz
 
