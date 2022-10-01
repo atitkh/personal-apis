1968df692994356469f8bb53a5860011d99bf98c
Atit Kharel <atit286@gmail.com>
2022-10-01 17:59:52 +0545

    updated client versions

--- routes/valorant/modules/api.js
+++ routes/valorant/modules/api.js
@@ -33,8 +33,8 @@ class API {
     this.user_id = null;
     this.access_token = null;
     this.entitlements_token = null;
-    this.user_agent = "RiotClient/43.0.1.4195386.4190634 rso-auth (Windows; 10;;Professional, x64)";
-    this.client_version = "release-05.00-shipping-6-725355";
+    this.user_agent = "RiotClient/58.0.0.4640299.4552318 %s (Windows;10;;Professional, x64)";
+    this.client_version = "release-05.06-shipping-6-765767";
     this.client_platform = {
       platformType: "PC",
       platformOS: "Windows",
