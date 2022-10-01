c5c96d15f0f17fcec577cb8c091f5dcbb5e22fa9
Atit Kharel <atit286@gmail.com>
2022-10-01 15:01:33 +0545

    added valorant api endpoint

--- routes/valorant/info.js
+++ routes/valorant/info.js
@@ -2,30 +2,33 @@ const router = require('express').Router();
 const fetch = require('node-fetch');
 const axios = require('axios');
 const verify = require('../auth/verifyToken');
+const Valorant = require('./valorant');
 
 router.get('/', (req, res) => {
     res.send('Welcome to Valorant API');
 });
 
-// get cookie
-router.get('/getCookie', async (req, res) => {
-    const postData = {
-        client_id: "play-valorant-web-prod",
-        nonce: "1",
-        redirect_uri: "https://playvalorant.com/opt_in",
-        response_type: "token id_token"
+// authenticate user
+router.get('/auth', async (req, res) => {
+    const valorantApi = new Valorant.API(Valorant.Regions.AsiaPacific);
+    const { username, password } = req.body;
+
+    if (!username || !password) {
+        return res.status(400).send('Please enter username and password');
     }
-    const cookie = await axios.post('https://auth.riotgames.com/api/v1/authorization', postData, {
-            headers: {
-                'Content-Type': 'application/json',
-                'User-Agent': 'RiotClient/43.0.1.4195386.4190634 rso-auth (Windows;10;;Professional, x64)',
-                "Access-Control-Allow-Origin": "*"
-            }
+
+    valorantApi.authorize(username, password).then(() => {
+        var data = {
+            "access_token": valorantApi.access_token,
+            "entitlements_token": valorantApi.entitlements_token,
+            "user_id": valorantApi.user_id,
+            "username": valorantApi.username,
+            "region": valorantApi.region
         }
-    ).then(response => {
-        res.send(response.data.response.parameters.cookie);
-    }).catch(err => {
-        res.send(err);
+
+        res.send(data);
+    }).catch((error) => {
+        res.status(403).send("Error Occured. Please try again.");
     });
 });
 
---
+++ routes/valorant/valorant.js
@@ -0,0 +1,14 @@
+module.exports = {
+    // Internal Game APIs
+    API: require("./modules/api"),
+    LocalRiotClientAPI: require("./modules/LocalRiotClientAPI"),
+  
+    // Third Party APIs
+    ContentAPI: require("./modules/ContentAPI"),
+  
+    // Static Content
+    Languages: require("./modules/languages"),
+    Maps: require("./modules/maps"),
+    Regions: require("./modules/regions"),
+    Tiers: require("./modules/tiers"),
+  };
