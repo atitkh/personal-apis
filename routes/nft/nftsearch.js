f3b360e850a822520c30c72b02a59140c10b1f9b
Atit Kharel <atit286@gmail.com>
2022-12-18 14:43:34 +0545

    increased timeout

--- routes/nft/nftsearch.js
+++ routes/nft/nftsearch.js
@@ -287,7 +287,7 @@ function ipfsPath(path) {
 
 async function sharpImg(url) {
     try {
-        let res = await axios({ url, responseType: "arraybuffer", keepAlive: true, timeout: 4000 });
+        let res = await axios({ url, responseType: "arraybuffer", keepAlive: true, timeout: 7000 });
         var buffer = Buffer.from(res.data, 'binary');
 
         var data = await sharp(buffer)
