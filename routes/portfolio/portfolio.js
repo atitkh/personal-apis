57d49b9a1a3192208cff0d036d784e3446a5bb69
Atit Kharel <atit286@gmail.com>
2024-09-13 00:11:47 -0500

    update: add support for markdown files, file structure change

--- routes/portfolio/portfolio.js
+++ routes/portfolio/portfolio.js
@@ -6,12 +6,26 @@ router.get('/', (req, res) => {
 });
 
 router.get('/atit', (req, res) => {
-    const atitPortfolio = require('./atit.json');
+    const atitPortfolio = require('./atit/atit.json');
     res.json(atitPortfolio);
 });
 
+router.get('/atit/md', (req, res) => {
+    const id = req.query.id;
+    const fs = require('fs');
+    const path = require('path');
+    const filePath = path.join(__dirname, 'atit', 'md', id + '.md');
+    
+    fs.readFile(filePath, 'utf8', (err, data) => {
+        if (err) {
+            return res.status(500).send('Error');
+        }
+        res.send(data);
+    });
+});
+
 router.get('/ashlesha', (req, res) => {
-    const ashleshaPortfolio = require('./ashlesha.json');
+    const ashleshaPortfolio = require('./ashlesha/ashlesha.json');
     res.json(ashleshaPortfolio);
 });
 
